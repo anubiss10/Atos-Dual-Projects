669c50eac59224f55dbb22b91df4de1c4e9e21aa
Juliandev <julianfelipearcecalvo@gmail.com>
2023-04-19 12:59:33 +0200

    Error Ejercicio3
    Daba error al abrir elarchivo

---
+++ Spring-MVC-Tareas/SpringBootWeb - jsp/src/main/java/com/app/julian/definitivo/DefinitivoApplication.java
@@ -0,0 +1,13 @@
+package com.app.julian.definitivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefinitivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefinitivoApplication.class, args);
+	}
+
+}
