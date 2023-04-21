939a4dfc2eab2653709c70f7c94c516148a66b5c
Juliandev <julianfelipearcecalvo@gmail.com>
2023-04-21 12:09:36 +0200

    Correcion ejercicio2

--- Spring-MVC-Tareas/Ejercicio2/src/main/java/com/app/julian/definitivo/controllers/IndexController.java
+++ Spring-MVC-Tareas/Ejercicio2/src/main/java/com/app/julian/definitivo/controllers/IndexController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-	@Value("${application.controller.titulo}")
-	private String titulo;
+	@Value("${application.controller.holamundo}")
+	private String holamundo;
     @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("titulo", this.titulo);
+        model.addAttribute("holamundo", this.holamundo);
         return "index";
     }
 }
---
+++ Spring-MVC-Tareas/Ejercicio2/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+
+</head>
+<body>
+
+	<c:out value="${holamundo"}></c:out>
+	</h1>
+	<h2>${holamundo}</h2>
+
+</body>
+</html>
