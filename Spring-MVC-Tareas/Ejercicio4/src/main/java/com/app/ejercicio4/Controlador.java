ed8aefb1af5b240c35ecccbd8d2cf9b11614d1a8
Juliandev <julianfelipearcecalvo@gmail.com>
2023-04-21 13:19:55 +0200

    Fin correciones

---
+++ Spring-MVC-Tareas/Ejercicio4/src/main/java/com/app/ejercicio4/Controlador.java
@@ -0,0 +1,12 @@
+package com.app.ejercicio4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Controlador {
+@RequestMapping
+	public String muestraPagina() {
+	return "paginaEjemplo";}
+	
+}
